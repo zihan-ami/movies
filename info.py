2cadbed615e5b72a8600e121da00544d0de8984d
zihan-ami <tasnimulzihan00@gmail.com>
2025-02-10 01:29:26 +0600

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7313565682:AAHomO7s9__CHBS28wg0t1rozQbeDKh
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6738816680').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/@thzihan") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002470545064'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://https://t.me/thetongwood')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/thetongwood')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002493277749').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
